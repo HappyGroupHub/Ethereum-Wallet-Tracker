9317597f194eb76717aa4b2e47b4fe8fa7d39000
LD <chiyc88@gmail.com>
2023-03-17 10:53:08 +0800

    Update - create a function to track latest txns

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 """This is the main file of the project."""
+import time
 
 import etherscan as eth
 import utilities as utils
 
 config = utils.read_config()
 wallet_address = config.get('wallet_address')
+check_interval = config.get('check_interval')
+offset = config.get('offset')
 latest_seen_block = 0
 latest_seen_normal_txn = str
 latest_seen_internal_txn = str
@@ -19,7 +22,7 @@ def init_latest_seen_txns():
     erc20_txn = eth.get_erc20_token_transfers(wallet_address, offset=1)[0]
     erc721_txn = eth.get_erc721_token_transfers(wallet_address, offset=1)[0]
     # erc1155_txn = eth.get_erc1155_token_transfers(wallet_address, offset=1)[0]
-    global latest_seen_block, latest_seen_normal_txn, latest_seen_internal_txn,\
+    global latest_seen_block, latest_seen_normal_txn, latest_seen_internal_txn, \
         latest_seen_erc20_txn, latest_seen_erc721_txn, latest_seen_erc1155_txn
     latest_seen_normal_txn = normal_txn['hash']
     latest_seen_internal_txn = internal_txn['hash']
@@ -29,13 +32,41 @@ def init_latest_seen_txns():
     for txn in [normal_txn, internal_txn, erc20_txn, erc721_txn]:
         if int(txn['blockNumber']) > latest_seen_block:
             latest_seen_block = int(txn['blockNumber'])
+    time.sleep(check_interval)
+
+
+def track_latest_txns():
+    global latest_seen_block, latest_seen_normal_txn, latest_seen_internal_txn, \
+        latest_seen_erc20_txn, latest_seen_erc721_txn, latest_seen_erc1155_txn
+    normal_txns = eth.get_normal_transactions(wallet_address, start_block=latest_seen_block,
+                                              offset=offset)
+    internal_txns = eth.get_internal_transactions(wallet_address, start_block=latest_seen_block,
+                                                  offset=offset)
+    erc20_txns = eth.get_erc20_token_transfers(wallet_address, start_block=latest_seen_block,
+                                               offset=offset)
+    erc721_txns = eth.get_erc721_token_transfers(wallet_address, start_block=latest_seen_block,
+                                                 offset=offset)
+    # erc1155_txns = eth.get_erc1155_token_transfers(wallet_address, start_block=latest_seen_block,
+    #                                                offset=offset)
+
+    txns_info = [
+        ("normal", normal_txns, latest_seen_normal_txn),
+        ("internal", internal_txns, latest_seen_internal_txn),
+        ("erc20", erc20_txns, latest_seen_erc20_txn),
+        ("erc721", erc721_txns, latest_seen_erc721_txn)
+        # ("erc1155", erc1155_txns, latest_seen_erc1155_txn)
+    ]
+    new_txns = []
+    for txns_name, txns_type, latest_seen_txn in txns_info:
+        for txn in txns_type:
+            if not txn['hash'] == latest_seen_txn:
+                new_txns.append(txn)
+            else:
+                break
+    new_txns = sorted(new_txns, key=lambda txn: txn['timeStamp'])
+    latest_seen_block = int(new_txns[-1]['blockNumber'])
 
 
 if __name__ == '__main__':
     init_latest_seen_txns()
-    print(f'Latest seen normal txn hash: {latest_seen_normal_txn}')
-    print(f'Latest seen internal txn hash: {latest_seen_internal_txn}')
-    print(f'Latest seen erc20 txn hash: {latest_seen_erc20_txn}')
-    print(f'Latest seen erc721 txn hash: {latest_seen_erc721_txn}')
-    # print(f'Latest seen erc1155 txn hash: {latest_seen_erc1155_txn}')
-    print(f'Latest seen block number: {latest_seen_block}')
+    track_latest_txns()
--- utilities.py
+++ utilities.py
@@ -20,6 +20,17 @@ etherscan_api_key: ''
 
 # Paste the wallet address you want to track here.
 wallet_address: ''
+
+# Time interval (in seconds) at which the program checks for new transactions on the tracked wallet addresses.
+# Noted that Etherscan API has a rate limit per second and day!
+# The default value is 15 seconds.
+check_interval: 15
+
+# The number of transactions to fetch per check.
+# A smaller offset value will result in faster API response / process times.
+# But may result in missing transactions if many transactions occur within a short time period.
+# The default value is 10.
+offset: 10
 """
                 )
     sys.exit()
@@ -43,7 +54,9 @@ def read_config():
             data = yaml.load(f, Loader=SafeLoader)
             config = {
                 'etherscan_api_key': data['etherscan_api_key'],
-                'wallet_address': data['wallet_address']
+                'wallet_address': data['wallet_address'],
+                'check_interval': data['check_interval'],
+                'offset': data['offset']
             }
             return config
     except (KeyError, TypeError):
